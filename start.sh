585c8b5fbabae40903a67db71a6a795b0b85e033
lyfsn <dev.wangyu@proton.me>
2024-06-13 11:19:04 +0800

    up

--- start.sh
+++ start.sh
@@ -13,26 +13,6 @@ nohup \
   > output.log 2>&1 &
 
 
-nohup \
-  ./run.sh \
-  -t "tests/" \
-  -c "nethermind,geth,reth,erigon,besu" \
-  -r 8 \
-  -o "results" \
-  -s 512,1024 \
-  > output.log 2>&1 &
-
-nohup \
-  ./run.sh \
-  -t "tests/" \
-  -c "nethermind,geth,reth,erigon,besu" \
-  -r 8 \
-  -o "results" \
-  -s 1,32,64,256,512,1024,2048 \
-  > output.log 2>&1 &
-
-
-
 
 
 ./runSpeed.sh \
@@ -40,46 +20,12 @@ nohup \
   -c "nethermind,geth,reth,erigon,besu" \
   -r 1 \
   -o "results" \
-  -s 1,2,3,4,5,6
+  -s 1
 
 
-
-
-nohup \
-  ./run.sh \
+./runMemory.sh \
   -t "tests/" \
   -c "nethermind,geth,reth,erigon,besu" \
-  -r 1 \
-  -o "results" \
-  -s 1,2,3 \
-  > output.log 2>&1 &
-
-./run.sh \
-  -t "tests/" \
-  -c "nethermind" \
-  -r 1 \
-  -o "results" \
-  -s 1,10
-
-
-./run.sh \
-  -t "tests/" \
-  -c "geth" \
-  -r 1 \
-  -o "results" \
-  -s 1000
-
-./run.sh \
-  -t "tests/" \
-  -c "erigon" \
-  -r 4 \
-  -o "results" \
-  -s 1,100,1000
-
-
-./run.sh \
-  -t "tests/" \
-  -c "besu" \
   -r 1 \
   -o "results" \
   -s 1
