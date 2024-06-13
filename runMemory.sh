b424d4479256ea1545024b6d5d9797b46f86f32a
lyfsn <dev.wangyu@proton.me>
2024-06-13 14:05:42 +0800

    up

--- runMemory.sh
+++ runMemory.sh
@@ -6,7 +6,7 @@ CLIENTS="nethermind,geth,reth"
 RUNS=8
 IMAGES="default"
 OUTPUT_DIR="results"
-SIZES=("1" "100" "1000")
+SIZES=("1" "64" "512")
 
 # Parse command line arguments
 while getopts "t:c:r:i:o:s:" opt; do
@@ -25,16 +25,17 @@ done
 IFS=',' read -ra CLIENT_ARRAY <<< "$CLIENTS"
 IFS=',' read -ra IMAGE_ARRAY <<< "$IMAGES"
 
-OUTPUT_DIR="$OUTPUT_DIR/speed"
+OUTPUT_DIR="$OUTPUT_DIR/memory"
 mkdir -p "$OUTPUT_DIR"
+mkdir -p "$TEST_PATH/tmp"
 
 # Install dependencies
+echo "[INFO] Installing dependencies..."
 pip install -r requirements.txt
 apt install -y jq
-
 python3 computer_specs.py --output_folder $OUTPUT_DIR
+echo "[INFO] Dependencies installed."
 
-# Function to monitor and record peak memory usage of a Docker container
 monitor_memory_usage() {
   local container_name=$1
   local output_file=$2
@@ -44,11 +45,9 @@ monitor_memory_usage() {
   {
     while :; do
       if [ "$(docker ps -q -f name=$container_name)" ]; then
-        # Extract memory usage and remove any potential units (like MiB, GiB)
         memory=$(docker stats --no-stream --format "{{.MemUsage}}" $container_name | awk '{print $1}')
-        echo "Raw memory usage: $memory"  # Debug output
+        echo "[DEBUG] Raw memory usage: $memory"  # Debug output
 
-        # Convert memory usage to MiB if necessary
         if [[ $memory == *MiB ]]; then
           memory=$(echo $memory | sed 's/[^0-9.]//g')
         elif [[ $memory == *GiB ]]; then
@@ -58,20 +57,18 @@ monitor_memory_usage() {
           memory=0
         fi
 
-        echo "Converted memory usage in MiB: $memory"  # Debug output
+        echo "[DEBUG] Converted memory usage in MiB: $memory"  # Debug output
 
         if (( $(echo "$memory > $max_memory" | bc -l) )); then
           max_memory=$memory
         fi
-        echo "$max_memory" > "$output_file"  # Write the max memory to the file in each iteration
+        echo "$max_memory" > "$output_file"
       fi
       sleep 0.1
     done
   } &
   echo $!
 }
-
-mkdir -p $TEST_PATH/tmp
 
 start_monitoring() {
   local client=$1
@@ -84,62 +81,66 @@ start_monitoring() {
     mem_output_file="${OUTPUT_DIR}/${client}_${run}_first_${size}M.txt"
     mem_pid=$(monitor_memory_usage "gas-execution-client-sync" "$mem_output_file")
   fi
-  echo "Started memory monitoring with PID $mem_pid"
+  echo "[INFO] Started memory monitoring with PID $mem_pid"
 }
 
 stop_monitoring() {
   if [ -n "$mem_pid" ]; then
     kill $mem_pid
-    echo "Stopped memory monitoring with PID $mem_pid"
+    echo "[INFO] Stopped memory monitoring with PID $mem_pid"
   fi
 }
 
-# Outer loop
+clean_up() {
+  echo "[INFO] Cleaning up containers and data..."
+  docker stop gas-execution-client gas-execution-client-sync
+  docker rm gas-execution-client gas-execution-client-sync
+  docker container prune -f
+  sudo rm -rf execution-data
+  echo "[INFO] Cleanup completed."
+}
+
 for size in "${SIZES[@]}"; do
-  echo "=== Running benchmarks for size ${size}M ==="
-  
-  echo "Calculating new size for $size"
+  echo "======================================"
+  echo "[INFO] Running benchmarks for size ${size}M"
+  echo "======================================"
+
+  echo "[INFO] Calculating new size for $size"
   new_size=$(echo "scale=2; ($size / 1.2 + 0.5)/1" | bc)
   if [ $? -ne 0 ]; then
-    echo "Error calculating new size with bc"
+    echo "[ERROR] Error calculating new size with bc"
     exit 1
   fi
-  echo "New size calculated: $new_size"
+  echo "[INFO] New size calculated: $new_size"
 
-  # Generate chainspec, genesis, and besu files
+  echo "[INFO] Generating chainspec, genesis, and besu files..."
   python3 generate_chainspec.py $TEST_PATH/chainspec.json $TEST_PATH/tmp/chainspec.json $new_size
   python3 generate_genesis.py $TEST_PATH/genesis.json $TEST_PATH/tmp/genesis.json $new_size
   python3 generate_besu.py $TEST_PATH/besu.json $TEST_PATH/tmp/besu.json $new_size
 
-  docker stop gas-execution-client
-  docker stop gas-execution-client-sync
-  docker rm gas-execution-client
-  docker rm gas-execution-client-sync
+  clean_up
 
-  # Run benchmarks
   for run in $(seq 1 $RUNS); do
     for I in "${!CLIENT_ARRAY[@]}"; do
-      echo "=== Run round $run - Client ${CLIENT_ARRAY[$I]} - Image ${IMAGE_ARRAY[$I]} ==="
+      echo "--------------------------------------"
+      echo "[INFO] Run round $run - Client ${CLIENT_ARRAY[$I]} - Image ${IMAGE_ARRAY[$I]}"
+      echo "--------------------------------------"
 
       client="${CLIENT_ARRAY[$I]}"
       image="${IMAGE_ARRAY[$I]}"
 
       cd "scripts/$client"
-      docker compose down
-      docker stop gas-execution-client
-      docker stop gas-execution-client-sync
-      docker rm gas-execution-client
-      docker rm gas-execution-client-sync
-      sudo rm -rf execution-data
+      docker compose down --remove-orphans
+      clean_up
       cd ../..
 
       start_monitoring $client $run $size &
 
       if [ -z "$image" ]; then
-        echo "Image input is empty, using default image."
+        echo "[INFO] Image input is empty, using default image."
         python3 setup_node.py --client $client
       else
-        echo "Using provided image: $image for $client"
+        echo "[INFO] Using provided image: $image for $client"
         python3 setup_node.py --client $client --image $image
       fi
 
@@ -152,25 +153,22 @@ for size in "${SIZES[@]}"; do
       start_monitoring $client $run $size &
 
       if [ -z "$image" ]; then
-        echo "Image input is empty, using default image."
+        echo "[INFO] Image input is empty, using default image."
         python3 setup_node.py --client $client --second-start
       else
-        echo "Using provided image: $image for $client"
+        echo "[INFO] Using provided image: $image for $client"
         python3 setup_node.py --client $client --image $image --second-start
       fi
 
       stop_monitoring
 
       cd "scripts/$client"
-      docker compose down
-      docker stop gas-execution-client
-      docker stop gas-execution-client-sync
-      docker rm gas-execution-client
-      docker rm gas-execution-client-sync
-      sudo rm -rf execution-data
+      docker compose down --remove-orphans
+      clean_up
       cd ../..
     done
   done
 done
 
-python3 report.py
+python3 report_memory.py --resultsPath $OUTPUT_DIR
+echo "[INFO] Benchmarking completed and report generated."
