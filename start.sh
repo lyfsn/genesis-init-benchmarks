3c903f07511602b9fa8b756378caea15af4340ee
lyfsn <dev.wangyu@proton.me>
2024-06-13 15:44:04 +0800

    up

--- start.sh
+++ start.sh
@@ -8,37 +8,16 @@ nohup \
   -t "tests/" \
   -c "nethermind,geth,reth,erigon,besu" \
   -r 8 \
-  -o "results" \
+  -o "results/speed" \
   -s 1,32,64,256,512,1024,2048 \
   > output.log 2>&1 &
 
-
-./runSpeed.sh \
-  -t "tests/" \
-  -c "nethermind" \
-  -r 1 \
-  -o "results/speed" \
-  -s 1
-
-
-./runMemory.sh \
-  -t "tests/" \
-  -c "nethermind" \
-  -r 1 \
-  -o "results/memory" \
-  -s 1
-
-./runMemory.sh \
+nohup \
+  ./runMemory.sh \
   -t "tests/" \
   -c "nethermind,geth,reth,erigon,besu" \
-  -r 1 \
+  -r 8 \
   -o "results/memory" \
-  -s 100
+  -s 1,32,64,256,512,1024,2048 \
+  > output.log 2>&1 &
 
-
-./runMemory.sh \
-  -t "tests/" \
-  -c "geth" \
-  -r 1 \
-  -o "results/memory" \
-  -s 1000
