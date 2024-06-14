65af6a4ef570c539a030215ee661714a8f322492
lyfsn <dev.wangyu@proton.me>
2024-06-14 11:53:35 +0800

    update

--- runMemory.sh
+++ runMemory.sh
@@ -25,56 +25,78 @@ done
 IFS=',' read -ra CLIENT_ARRAY <<< "$CLIENTS"
 IFS=',' read -ra IMAGE_ARRAY <<< "$IMAGES"
 
-mkdir -p "$OUTPUT_DIR"
-mkdir -p "$TEST_PATH/tmp"
+mkdir -p "$OUTPUT_DIR" "$TEST_PATH/tmp"
 
 # Install dependencies
-echo "[INFO] Installing dependencies..."
-pip install -r requirements.txt
-apt install -y jq
-python3 computer_specs.py --output_folder $OUTPUT_DIR
-echo "[INFO] Dependencies installed."
+install_dependencies() {
+  echo "[INFO] Installing dependencies..."
+  pip install -r requirements.txt
+  apt install -y jq
+  python3 computer_specs.py --output_folder "$OUTPUT_DIR"
+  echo "[INFO] Dependencies installed."
+}
+
+install_dependencies
+
+wait_for_container() {
+  local container_name=$1
+  local max_retries=7200
+  local retry_count=0
+  local wait_time=0.5
+
+  echo "[INFO] Waiting for container $container_name to start..."
+  while [ $retry_count -lt $max_retries ]; do
+    if docker ps -q -f name="$container_name" &>/dev/null; then
+      echo "[INFO] Container $container_name is running."
+      return 0
+    fi
+    retry_count=$((retry_count + 1))
+    sleep $wait_time
+  done
+
+  echo "[ERROR] Container $container_name did not start after $max_retries retries."
+  return 1
+}
+
+check_container_running() {
+  local container_name=$1
+  local max_retries=240
+  local retry_count=0
+  local max_wait_time=0.5
+
+  while [ $retry_count -lt $max_retries ]; do
+    if [ -z "$(docker ps -q -f name=$container_name)" ]; then
+      echo "[ERROR] Container $container_name has stopped unexpectedly. Retrying... ($((retry_count + 1))/$max_retries)"
+      retry_count=$((retry_count + 1))
+      sleep $max_wait_time
+    else
+      return 0
+    fi
+  done
+
+  echo "[ERROR] Container $container_name has stopped unexpectedly after $max_retries retries."
+  return 1
+}
 
 check_initialization_completed() {
   local client=$1
   local log_entry=$2
-  local container_name="gas-execution-client"
-  local max_retries=7200
-  local retry_count=0
-  local wait_time=0.5 
-  local max_wait_time=120 
-  local container_check_retries=12
-  local container_retry_count=0
+  local container_name=$3
 
-  check_container_running() {
-    while [ $container_retry_count -lt $container_check_retries ]; do
-      if [ -z "$(docker ps -q -f name=$container_name)" ]; then
-        echo "[ERROR] Container $container_name has stopped unexpectedly. Retrying... ($((container_retry_count + 1))/$container_check_retries)"
-        container_retry_count=$((container_retry_count + 1))
-        sleep $max_wait_time
-      else
-        return 0
-      fi
-    done
-    echo "[ERROR] Container $container_name has stopped unexpectedly after $container_check_retries retries."
-    return 1
-  }
-
-  check_container_running
-  if [ $? -ne 0 ]; then
+  if ! wait_for_container "$container_name"; then
     return 1
   fi
 
-  echo "[INFO] Container $container_name has started."
+  local max_retries=7200
+  local retry_count=0
+  local wait_time=0.5
 
-  retry_count=0
   echo "[INFO] Waiting for log entry: '$log_entry' in $container_name..."
-  until docker logs $container_name 2>&1 | grep -q "$log_entry"; do
+  until docker logs "$container_name" 2>&1 | grep -q "$log_entry"; do
     sleep $wait_time
-    retry_count=$((retry_count+1))
+    retry_count=$((retry_count + 1))
 
-    check_container_running
-    if [ $? -ne 0 ]; then
+    if ! check_container_running "$container_name"; then
       return 1
     fi
 
@@ -93,65 +115,25 @@ start_monitoring() {
   local run=$2
   local size=$3
   local suffix=$4
-  local container_name
+  local container_name=$5
   local max_memory=0
-  local max_retries=7200
-  local retry_count=0
   local wait_time=0.1
-  local max_wait_time=1000
-  local container_check_retries=12
-  local container_retry_count=0
-  local unique_id="monitor_$client_$run_$size"
-  
-  if [ "$client" = "nethermind" ] || [ "$client" = "besu" ]; then
-    container_name="gas-execution-client"
-  else
-    container_name="gas-execution-client-sync"
-  fi
   local mem_output_file="${OUTPUT_DIR}/${client}_${run}_${suffix}_${size}M.txt"
-
-  check_container_running() {
-    while [ $container_retry_count -lt $container_check_retries ]; do
-      if [ -z "$(docker ps -q -f name=$container_name)" ]; then
-        echo "[ERROR] Container $container_name has stopped unexpectedly. Retrying... ($((container_retry_count + 1))/$container_check_retries)"
-        container_retry_count=$((container_retry_count + 1))
-        sleep $max_wait_time
-      else
-        return 0
-      fi
-    done
-    echo "[ERROR] Container $container_name has stopped unexpectedly after $container_check_retries retries."
-    return 1
-  }
+  local unique_id="monitor_$client_$run_$size"
 
   {
-    echo "[INFO] Waiting for container $container_name to start..."
-    while [ $retry_count -lt $max_retries ]; do
-      if docker ps -q -f name="$container_name" &>/dev/null; then
-        echo "[INFO] Container $container_name is running. Starting memory monitoring."
-        break
-      fi
-      retry_count=$((retry_count + 1))
-      sleep $wait_time
-    done
-
-    if [ $retry_count -eq $max_retries ]; then
-      echo "[ERROR] Container $container_name did not start after $max_retries retries."
+    if ! wait_for_container "$container_name"; then
       exit 1
     fi
 
     while :; do
-      if ! check_container_running; then
+      if ! check_container_running "$container_name"; then
         exit 1
       fi
-      
+
       stats_output=$(docker stats --no-stream --format "{{.MemUsage}}" "$container_name")
-      
       mem_usage=$(echo "$stats_output" | awk '{print $1}' | tr -d '[:alpha:]')
       mem_unit=$(echo "$stats_output" | awk '{print $1}' | tr -d '[:digit:]')
-
-      echo "[DEBUG] Stats output: $stats_output"  # Debug output
-      echo "[DEBUG] Memory usage: $mem_usage $mem_unit"  # Debug output
 
       case $mem_unit in
         MiB) mem_usage_mib=$mem_usage ;;
@@ -161,8 +143,6 @@ start_monitoring() {
         *)   mem_usage_mib=0 ;;
       esac
 
-      echo "[DEBUG] Converted memory usage in MiB: $mem_usage_mib"  # Debug output
-
       if (( $(echo "$mem_usage_mib > $max_memory" | bc -l) )); then
         max_memory=$mem_usage_mib
       fi
@@ -170,7 +150,7 @@ start_monitoring() {
       echo "$max_memory" > "$mem_output_file"
       sleep $wait_time
     done
-  } & echo $! 
+  } & echo $!
 }
 
 stop_monitoring() {
@@ -180,6 +160,7 @@ stop_monitoring() {
     echo "[INFO] Stopped memory monitoring with PID $mem_pid"
     mem_pid=""
   fi
+
   unique_id_pattern="monitor_"
   pids=$(pgrep -f "$unique_id_pattern")
   if [ -n "$pids" ]; then
@@ -191,37 +172,30 @@ stop_monitoring() {
 
 container_exists() {
   local container_name=$1
-  if [ "$(docker ps -a -q -f name=$container_name)" ]; then
-    return 0
-  else
-    return 1
-  fi
+  [ "$(docker ps -a -q -f name=$container_name)" ]
 }
 
 clean_up() {
   echo "[INFO] Cleaning up containers and data..."
   stop_monitoring
-  if container_exists "gas-execution-client"; then
-    docker stop gas-execution-client
-    docker rm gas-execution-client
-  fi
-  if container_exists "gas-execution-client-sync"; then
-    docker stop gas-execution-client-sync
-    docker rm gas-execution-client-sync
-  fi
+  for container_name in gas-execution-client gas-execution-client-sync; do
+    if container_exists "$container_name"; then
+      docker stop "$container_name"
+      docker rm "$container_name"
+    fi
+  done
   docker container prune -f
   sudo rm -rf execution-data
   echo "[INFO] Cleanup completed."
 }
 
-trap clean_up EXIT # Ensure cleanup is called on script exit
+trap clean_up EXIT
 
 for size in "${SIZES[@]}"; do
   echo "======================================"
   echo "[INFO] Running benchmarks for size ${size}M"
   echo "======================================"
 
-  echo "[INFO] Calculating new size for $size"
   new_size=$(echo "scale=2; ($size / 1.2 + 0.5)/1" | bc)
   if [ $? -ne 0 ]; then
     echo "[ERROR] Error calculating new size with bc"
@@ -230,18 +204,14 @@ for size in "${SIZES[@]}"; do
   echo "[INFO] New size calculated: $new_size"
 
   echo "[INFO] Generating chainspec, genesis, and besu files..."
-  python3 generate_chainspec.py $TEST_PATH/chainspec.json $TEST_PATH/tmp/chainspec.json $new_size
-  python3 generate_genesis.py $TEST_PATH/genesis.json $TEST_PATH/tmp/genesis.json $new_size
-  python3 generate_besu.py $TEST_PATH/besu.json $TEST_PATH/tmp/besu.json $new_size
+  python3 generate_chainspec.py "$TEST_PATH/chainspec.json" "$TEST_PATH/tmp/chainspec.json" "$new_size"
+  python3 generate_genesis.py "$TEST_PATH/genesis.json" "$TEST_PATH/tmp/genesis.json" "$new_size"
+  python3 generate_besu.py "$TEST_PATH/besu.json" "$TEST_PATH/tmp/besu.json" "$new_size"
 
   clean_up
 
   for run in $(seq 1 $RUNS); do
     for I in "${!CLIENT_ARRAY[@]}"; do
-      echo "--------------------------------------"
-      echo "[INFO] Run round $run - Client ${CLIENT_ARRAY[$I]} - Image ${IMAGE_ARRAY[$I]}"
-      echo "--------------------------------------"
-
       client="${CLIENT_ARRAY[$I]}"
       image="${IMAGE_ARRAY[$I]}"
 
@@ -258,44 +228,21 @@ for size in "${SIZES[@]}"; do
       docker compose down --remove-orphans
       cd ../..
 
-      start_monitoring $client $run $size "first"
+      container_name="gas-execution-client"
+      [ "$client" != "nethermind" ] && [ "$client" != "besu" ] && container_name="gas-execution-client-sync"
 
-      if [ -z "$image" ]; then
-        echo "[INFO] Image input is empty, using default image."
-        python3 setup_node.py --client $client
-      else
-        echo "[INFO] Using provided image: $image for $client"
-        python3 setup_node.py --client $client --image $image
-      fi
-
-      check_initialization_completed $client "$log_entry"
-      if [ $? -ne 0 ]; then
-        echo "[ERROR] Initialization check failed for client $client"
-        stop_monitoring
-        continue
-      fi
+      start_monitoring "$client" "$run" "$size" "first" "$container_name"
+      [ -z "$image" ] && python3 setup_node.py --client "$client" || python3 setup_node.py --client "$client" --image "$image"
+      check_initialization_completed "$client" "$log_entry" "$container_name" || { stop_monitoring; continue; }
       stop_monitoring
 
       cd "scripts/$client"
       docker compose stop
       cd ../..
 
-      start_monitoring $client $run $size "second"
-
-      if [ -z "$image" ]; then
-        echo "[INFO] Image input is empty, using default image."
-        python3 setup_node.py --client $client --second-start
-      else
-        echo "[INFO] Using provided image: $image for $client"
-        python3 setup_node.py --client $client --image $image --second-start
-      fi
-
-      check_initialization_completed $client "$log_entry"
-      if [ $? -ne 0 ]; then
-        echo "[ERROR] Initialization check failed for client $client"
-        stop_monitoring
-        continue
-      fi
+      start_monitoring "$client" "$run" "$size" "second" "$container_name"
+      [ -z "$image" ] && python3 setup_node.py --client "$client" --second-start || python3 setup_node.py --client "$client" --image "$image" --second-start
+      check_initialization_completed "$client" "$log_entry" "$container_name" || { stop_monitoring; continue; }
       stop_monitoring
 
       cd "scripts/$client"
@@ -306,5 +253,5 @@ for size in "${SIZES[@]}"; do
   done
 done
 
-python3 report_memory.py --resultsPath $OUTPUT_DIR
+python3 report_memory.py --resultsPath "$OUTPUT_DIR"
 echo "[INFO] Benchmarking completed and report generated."
